bd47a22cf78bbbfe91beaa852d17193e6ff7d994
norsicl <lorisnorsic@hotmail.fr>
2014-12-16 22:19:56 +0100

    Update rtf.java

--- plugins/rtf/rtf.java
+++ plugins/rtf/rtf.java
@@ -4,9 +4,13 @@ import java.io.*;
 public class rtf {
 	private File fileEntree;
 	private File fileSortie;
-	private String REGEX = "\\{[^\\;]+\\;\\}|\\\\[^ ]+[ ]+|\\{|\\}|[0-9a-fA-F]{127}|[0-9a-fA-F]{12}";
-	private String REGEX2 = "^\\.*;\\";
-	private String REPLACE = "";
+
+	private String REGEX = "\\\\(par)\\W|\\\\(pard)";
+	private String REGEX2 = "\\{[^\\;]+\\;\\}|\\\\[^ ]+[ ]+|\\{|\\}|[0-9a-fA-F]{127}|[0-9a-fA-F]{12}";
+	private String REGEX3 = "^\\\\.*;\\\\|\\\\[^ ]+[\\w+\\d*]";
+	private String REGEX4 = ".*\\;.*[a-zA-a]*\\;";
+	private String REPLACE = "\n";
+	private String REPLACE2 = "";
 
 	public rtf(String in, String out){
 		this.fileEntree = new File(in);
@@ -23,31 +27,19 @@ public class rtf {
 		String line = br.readLine();
 		while((line=br.readLine())!=null){
 			String newline = line.replaceAll(this.REGEX,this.REPLACE);
+			newline = newline.replaceAll(this.REGEX2,this.REPLACE2);
+			newline = newline.replaceAll(this.REGEX3, this.REPLACE2);
+			newline = newline.replaceAll(this.REGEX4, this.REPLACE2);
 			bw.write(newline);
 		}
 		br.close();
 		bw.close();
 	}
 	
-	public void test2() throws IOException{
-		FileWriter fw2 = new FileWriter(this.fileSortie);
-		BufferedWriter bw2 = new BufferedWriter(fw2);
-		FileReader fr2 = new FileReader(this.fileEntree);
-		BufferedReader br2 = new BufferedReader(fr2);
-		String line = br2.readLine();
-		while((line=br2.readLine())!=null){
-			String newline = line.replaceAll(this.REGEX2,this.REPLACE);
-			bw2.write(newline);
-		}
-		br2.close();
-		bw2.close();
-	}
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		rtf test = new rtf("buffalo96.rtf","test.rtf");
 		test.test();
-		rtf test2 = new rtf("test.rtf", "test2.rtf");
-		test2.test2();
 	}
 }
