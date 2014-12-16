4d4479ded40d97451e6acda2ae9bf3f963c089f5
norsicl <lorisnorsic@hotmail.fr>
2014-12-16 09:14:42 +0100

    Update rtf.java

--- plugins/rtf/rtf.java
+++ plugins/rtf/rtf.java
@@ -1,15 +1,11 @@
 import java.io.*;
-import java.lang.Object;
-import java.util.*;
-import java.util.regex.*;
 
 
 public class rtf {
-	private static String destination = "final.txt";
 	private File fileEntree;
 	private File fileSortie;
 	private String REGEX = "\\{[^\\;]+\\;\\}|\\\\[^ ]+[ ]+|\\{|\\}|[0-9a-fA-F]{127}|[0-9a-fA-F]{12}";
-	private String REGEX2 = "^\\.*\\(\\)";
+	private String REGEX2 = "^\\.*;\\";
 	private String REPLACE = "";
 
 	public rtf(String in, String out){
@@ -17,7 +13,6 @@ public class rtf {
 		this.fileSortie = new File(out);
 		
 	}
-	
 	
 	//replace line by line with the regular expression
 	public void test() throws IOException{
@@ -32,27 +27,25 @@ public class rtf {
 		}
 		br.close();
 		bw.close();
-		
 	}
 	
 	public void test2() throws IOException{
-		FileWriter fw = new FileWriter(this.fileSortie);
-		BufferedWriter bw = new BufferedWriter(fw);
-		FileReader fr = new FileReader(this.fileEntree);
-		BufferedReader br = new BufferedReader(fr);
-		String line = br.readLine();
-		while((line=br.readLine())!=null){
+		FileWriter fw2 = new FileWriter(this.fileSortie);
+		BufferedWriter bw2 = new BufferedWriter(fw2);
+		FileReader fr2 = new FileReader(this.fileEntree);
+		BufferedReader br2 = new BufferedReader(fr2);
+		String line = br2.readLine();
+		while((line=br2.readLine())!=null){
 			String newline = line.replaceAll(this.REGEX2,this.REPLACE);
-			bw.write(newline);
+			bw2.write(newline);
 		}
-		br.close();
-		bw.close();
-		
+		br2.close();
+		bw2.close();
 	}
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		rtf test = new rtf("korea_final.rtf","test.rtf");
+		rtf test = new rtf("buffalo96.rtf","test.rtf");
 		test.test();
 		rtf test2 = new rtf("test.rtf", "test2.rtf");
 		test2.test2();
